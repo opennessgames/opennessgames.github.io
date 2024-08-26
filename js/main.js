74f98f8842369ced89774d82723e94ec2e97fed2
Yatwohee <jisanduo@outlook.com>
2024-08-26 23:45:48 +0800

    加入开学音效

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
-//唉...............
-let background = new Audio("../background_music.mp3");
-background.loop = true;
-background.play();
-setTimeout(() => {
-    window.location.assign('../construct/');
-}, 1000);
+/* 
+    By : yzx
+    Time 2024年8月26日 下午 10:14:02 
+*/
+//播放背景音乐
+function PlayMusic() {
+    var TenThousandBgMusic = new Audio("../audio/bg.mp3");//太棒啦,我有对象了
+    TenThousandBgMusic.loop = true;//循环播放
+    TenThousandBgMusic.play();
+}
+/* 闫仔豪我草你妈,傻逼玩意重开吧 */
+PlayMusic();
---
+++ xiningyouxiduan/js/main.js
@@ -0,0 +1,7 @@
+//唉...............
+let background = new Audio("../xiningyouxiduan/background_music.mp3");
+background.loop = true;
+background.play();
+/* setTimeout(() => {
+    window.location.assign('../construct/');
+}, 1000); */
